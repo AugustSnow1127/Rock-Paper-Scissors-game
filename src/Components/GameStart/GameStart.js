8512e5bbe6e2acc2fae1224b908523048873e897
Angus Chang <angus_chang@wistron.com>
2024-01-30 17:17:40 +0800

    feat: add rock/paper/scissor btn and layout

--- src/Components/GameStart/GameStart.js
+++ src/Components/GameStart/GameStart.js
@@ -1,9 +1,28 @@
 import React from 'react'
-import { MainContainer } from './style'
+import triangle from '../../static/images/bg-triangle.svg'
+import rock from '../../static/images/icon-rock.svg'
+import paper from '../../static/images/icon-paper.svg'
+import scissors from '../../static/images/icon-scissors.svg'
+import {
+  MainContainer,
+  UpperTriangleLayout,
+  LowerTriangleLayout,
+  TriangleImg,
+  GameButton,
+} from './style'
 
 const GameStart = () => {
   return (
-    <MainContainer>GameStart</MainContainer>
+    <MainContainer>
+      <TriangleImg src={triangle} alt="triangle" />
+      <UpperTriangleLayout>
+        <GameButton icon={<img src={paper} alt="paper" />} />
+        <GameButton icon={<img src={scissors} alt="scissors" />} />
+      </UpperTriangleLayout>
+      <LowerTriangleLayout>
+      <GameButton icon={<img src={rock} alt="rock" />} />
+      </LowerTriangleLayout>
+    </MainContainer>
   )
 }
 
--- src/Components/GameStart/style.js
+++ src/Components/GameStart/style.js
@@ -1,6 +1,46 @@
 import styled from "styled-components";
+import { Button } from "antd";
 
 export const MainContainer = styled.div`
   position: relative;
   top: 50px;
+  width: 500px;
+  height: 500px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const TriangleImg = styled.img`
+  position: absolute;
+`;
+
+export const UpperTriangleLayout = styled.div`
+  height: 50%;
+  width: 100%;
+  position: relative;
+  top: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 50px;
+  box-sizing: border-box;
+`;
+
+export const LowerTriangleLayout = styled.div`
+  height: 50%;
+  width: 100%;
+  position: relative;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-sizing: border-box;
+`;
+
+export const GameButton = styled(Button)`
+  width: 150px !important;
+  height: 150px;
+  border-radius: 50%;
 `;
